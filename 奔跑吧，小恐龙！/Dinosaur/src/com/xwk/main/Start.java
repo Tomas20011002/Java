80f054897292cae2540456729d952cf5db512e8d
Tomas Wilson <63546166+Tomas20011002@users.noreply.github.com>
2021-05-17 13:28:30 +0800

    Add files via upload

---
+++ 奔跑吧，小恐龙！/Dinosaur/src/com/xwk/main/Start.java
@@ -0,0 +1,17 @@
+package com.xwk.main;
+
+import com.xwk.view.MainFrame;
+
+/**
+ *
+ * @author ĞÏÎµÀ¤ 
+ * 2021Äê5ÔÂ16ÈÕ
+ *
+ */
+public class Start {
+
+	public static void main(String[] args) {
+		MainFrame frame = new MainFrame();
+		frame.setVisible(true);
+	}
+}
